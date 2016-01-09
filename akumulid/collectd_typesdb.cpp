a114673a9062ac1ccfcca74807615415385c269b
Claudius Zingerli <gitmail@zeuz.ch>
2016-01-10 00:56:08 +0100

    types.db parser is feature complete; testing needed

--- akumulid/collectd_typesdb.cpp
+++ akumulid/collectd_typesdb.cpp
@@ -1,80 +1,16 @@
+//
+// Copyright 2016 Claudius Zingerli <akumulimail@zeuz.ch>
+// Distributed under Apache License Version 2.0
+//
+// Parse types.db for importing collectd data into akumuli
+//
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
 #include <boost/algorithm/string.hpp>
-
-using std::cout;
-using std::endl;
-using std::flush;
-
-struct TypesDBentry
-{
-	std::string name_;
-	typedef enum
-	{
-		GAUGE,COUNTER,DCOUNTER,DERIVE,DDERIVE,ABSOLUTE,COMPUTE
-	} eMappedName;
-	eMappedName mapped_name_;
-	double min_,max_;
-
-	static const char* str_from_mapped_name(eMappedName p_name)
-	{
-		switch (p_name)
-		{
-			#define CASE_STR_RET_STR(x) case x: return #x; break
-			default:
-			CASE_STR_RET_STR(GAUGE);
-			CASE_STR_RET_STR(COUNTER);
-			CASE_STR_RET_STR(DCOUNTER);
-			CASE_STR_RET_STR(DERIVE);
-			CASE_STR_RET_STR(DDERIVE);
-			CASE_STR_RET_STR(ABSOLUTE);
-			CASE_STR_RET_STR(COMPUTE);
-		}
-	}
-	static eMappedName mapped_name_from_str(std::string const&p_str)
-	{
-		//Translates p_str to eMappedName value; Defaults to GAUGE
-		if (boost::iequals(p_str,"gauge"))
-		{
-			return TypesDBentry::GAUGE;
-		} else
-		if (boost::iequals(p_str,"counter"))
-		{
-			return TypesDBentry::COUNTER;
-		} else
-		if (boost::iequals(p_str,"dcounter"))
-		{
-			return TypesDBentry::DCOUNTER;
-		} else
-		if (boost::iequals(p_str,"derive"))
-		{
-			return TypesDBentry::DERIVE;
-		} else
-		if (boost::iequals(p_str,"dderive"))
-		{
-			return TypesDBentry::DDERIVE;
-		} else
-		if (boost::iequals(p_str,"absolute"))
-		{
-			return TypesDBentry::ABSOLUTE;
-		} else
-		if (boost::iequals(p_str,"compute"))
-		{
-			return TypesDBentry::COMPUTE;
-		} else {
-			return TypesDBentry::GAUGE;
-		}
-	}
-};
-
-struct TypesDB: std::map<std::string, std::vector<TypesDBentry> >
-{
-	void load(const std::string &p_path);      // Loads a whole types.db file 
-	void parse_line(const std::string p_line); // Parses a single line
-	void dump(std::ostream &p_out) const;      // Dumps the current contents to p_out
-};
+#include "collectd_typesdb.h"
 
 void TypesDB::load(const std::string &p_path)
 {
@@ -160,10 +96,3 @@ void TypesDB::dump(std::ostream &p_out) const
 	}
 }
 
-int main()
-{
-	//Load file
-	TypesDB typesdb;
-	typesdb.load("types_test.db");
-	typesdb.dump(cout);
-}
